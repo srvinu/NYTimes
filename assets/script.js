3d8c8572b0700fa9555881709e85fb6afc3046d3
Brent Abruzese <brent.abruzese@gmail.com>
2019-03-23 13:18:55 -0400

    added vinod's search button function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-var queryURL = 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + " &api-key=WRrl018IfzEP8wsGN2FNnsle1kwhLj5S"
+
+$("#search").on("click", function() {
+    event.preventDefault();
+    var searchTerm = $("#searchTerm").val().trim();
+    var numOfRecords = $('#numOfRecords').val().trim();
+    var startYear = $('#startYear').val().trim();
+    var endYear = $('#endYear').val().trim();
+   });
 
 $.ajax({
     url: queryURL,
