c7d54251c1100eede56d3b0797a31f00ae3a554f
Dan Correa <dancorrea3@gmail.com>
2019-03-23 13:44:18 -0400

    index mispelling

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,5 @@ var queryURL =
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function (response) {
-
-    console.log(response);
-    var results = response.data;
-
-    
-    for (var i = 0; i < results.length; i++) {
-      var = $("<div>");
-      var rating = results[i].rating;
-      var p = $("<p>").text("Rating: " + rating);
-      var = $("<img>");
-      .attr("src", results[i].images.fixed_height.url);
-      .prepend(p);
-      .prepend();
-
+})
     
